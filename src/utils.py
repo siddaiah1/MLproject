8641fc9782019d8e24248b04ad423a5c145027fc
siddaiah1 <siddaiahnandyala1@gmail.com>
2026-01-13 21:40:52 +0530

    model trainer

--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,7 @@ import sys
 import pandas as pd
 import numpy as np
 import dill 
+from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error, mean_squared_error
 
 
 from src.exception import CustomException
@@ -21,4 +22,70 @@ def save_object(file_path,obj):
 
     except Exception as e:
         raise CustomException(e,sys) 
+
+
+def evaluate_model(X_train,y_train,X_test,y_test,models):
+    try:
+        report={}
+
+        for i in range(len(models)):
+            model=list(models.values())[i]
+            model.fit(X_train,y_train)
+
+            y_test_pred=model.predict(X_test)
+
+            test_model_score=r2_score(y_test,y_test_pred)
+
+            report[list(models.keys())[i]]=test_model_score
+
+        return report
+
+    except Exception as e:
+        raise CustomException(e,sys)
+    
+def model_metrics(true, predicted):
+    try :
+        mae = mean_absolute_error(true, predicted)
+        mse = mean_squared_error(true, predicted)
+        rmse = np.sqrt(mse)
+        r2_square = r2_score(true, predicted)
+        return mae, rmse, r2_square
+    except Exception as e:
+        logging.info('Exception Occured while evaluating metric')
+        raise CustomException(e,sys)
+    
+
+def print_evaluated_results(xtrain,ytrain,xtest,ytest,model):
+    try:
+        ytrain_pred = model.predict(xtrain)
+        ytest_pred = model.predict(xtest)
+
+        # Evaluate Train and Test dataset
+        model_train_mae , model_train_rmse, model_train_r2 = model_metrics(ytrain, ytrain_pred)
+        model_test_mae , model_test_rmse, model_test_r2 = model_metrics(ytest, ytest_pred)
+
+        # Printing results
+        print('Model performance for Training set')
+        print("- Root Mean Squared Error: {:.4f}".format(model_train_rmse))
+        print("- Mean Absolute Error: {:.4f}".format(model_train_mae))
+        print("- R2 Score: {:.4f}".format(model_train_r2))
+
+        print('----------------------------------')
+    
+        print('Model performance for Test set')
+        print("- Root Mean Squared Error: {:.4f}".format(model_test_rmse))
+        print("- Mean Absolute Error: {:.4f}".format(model_test_mae))
+        print("- R2 Score: {:.4f}".format(model_test_r2))
+    
+    except Exception as e:
+        logging.info('Exception occured during printing of evaluated results')
+        raise CustomException(e,sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path,'rb') as file_obj:
+            return dill.load(file_obj)
+    except Exception as e:
+        logging.info('Exception Occured in load_object function utils')
+        raise CustomException(e,sys)
     
